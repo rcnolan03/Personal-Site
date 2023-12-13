7f842a4927bb7e2ae18e56850319d95d77b4f19b
rcnolan <rcnolan@vt.edu>
2023-12-12 22:07:13 -0500

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    colors: {
+      ...colors,
+      primary: colors.blue,
+      secondary: colors.red,
+    },
+  },
+  plugins: [],
+}
+export default config
